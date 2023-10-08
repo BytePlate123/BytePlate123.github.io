b297d17fd89c61f69c65b8e78708a8a829f51c62
TessyJr <christopher.nathanael1217@gmail.com>
2023-10-08 15:22:02 +0700

    mix: setup repository and added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        abrilFatface: ["Abril Fatface", ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        // Add your custom colors here
+        "custom-pink": "#F5CDB2",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
